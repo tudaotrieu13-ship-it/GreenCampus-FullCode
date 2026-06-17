570a442734ca37bfa67fb2479753ea44e7f9fc55
Dao Trieu Tu <tudaotrieu13@gmail.com>
2026-06-17 21:30:32 +0700

    Update API URL for Production

--- fontend/src/config/api.js
+++ fontend/src/config/api.js
@@ -1,2 +1,9 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-export const BACKEND_ORIGIN = import.meta.env.VITE_BACKEND_ORIGIN || 'http://localhost:5000';
+//local
+//export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+//export const BACKEND_ORIGIN = import.meta.env.VITE_BACKEND_ORIGIN || 'http://localhost:5000';
+
+
+//production 
+export const API_URL = import.meta.env.VITE_API_URL || 'https://greencampus-backend-tu-f3a3bgbsa7d8aac7.southeastasia-01.azurewebsites.net/api';
+export const BACKEND_ORIGIN = import.meta.env.VITE_BACKEND_ORIGIN || 'https://greencampus-backend-tu-f3a3bgbsa7d8aac7.southeastasia-01.azurewebsites.net';
+
